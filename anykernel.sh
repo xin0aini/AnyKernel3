36abc829c5d9df492b5f6db807ccaea517898256
神楽坂忻 <38414925+xin0aini@users.noreply.github.com>
2024-04-21 17:56:48 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=kernel by 阿邪 @Coolapk && LibXZR @Github
+kernelsu Build and package by 阿邪 
 do.devicecheck=0
 do.modules=0
 do.systemless=0
